aef2c7885e51061ce90931f34f0f916198b9f9e0
lemen <les_lemen@ymail.com>
2012-11-08 11:48:34 +0100

    first commit

---
+++ hello_world.java
@@ -0,0 +1,19 @@
+/*
+*****************************************************************
+*	    Name: Peter Faber, Clemens Mayer, Han Ngoc Phan	
+*	   Datum: Tue Oct 30 18:33:09 CET 2012			
+*	 Aufgabe: Hallo Welt				
+*	 Version: 1.0						
+*	Funktion: Hello World					
+*****************************************************************
+*/
+
+class hello_world
+{
+   public static void main(String[] args)
+   {
+
+	System.out.println("Hello World");
+
+   }
+}
